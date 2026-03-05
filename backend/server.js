be48b68afc5bc1e74e7af6f543bc0a91e562d0d8
Naresh Golla <mrgollanaresh95@gmail.com>
2026-03-05 18:40:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://citisolve-fullstack-frontent.onrender.com",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Static folder for uploads
@@ -26,6 +30,8 @@ app.use("/api/complaints", complaintRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
