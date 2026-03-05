ddb3be477e2c6c88dfc62fec334a033bbcb4b275
mrnaresh95 <mrgollanaresh95@gmail.com>
2026-03-05 17:56:58 +0530

    First-coomit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://comaplaint-management-system.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Static folder for uploads
@@ -27,6 +32,12 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => {
+const PORT= process.env.PORT || 5000
+
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Citisolve Backend is Running 🚀");
 });
